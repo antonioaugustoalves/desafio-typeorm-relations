0525cd4c920f7e111240ace1ddab62b165b49ba5
Antonio A. Alves <antonioaugusto.alves1981@gmail.com>
2020-11-27 15:21:07 -0300

    Erro em ProductsRepository

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -6,23 +6,30 @@ import {
   UpdateDateColumn,
   OneToMany,
 } from 'typeorm';
-//Continuar a partir de 49:18 no video
-//https://www.youtube.com/watch?v=nYcdmLIPr7s
+
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('products')
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column('decimal')
   price: number;
 
+  @Column('int')
   quantity: number;
 
+  //@OneToMany(()=> OrdersProducts, order_products => order_products.product)
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import Product from '../infra/typeorm/entities/Product';
 import IProductsRepository from '../repositories/IProductsRepository';
+import ProductsRepository from '../infra/typeorm/repositories/ProductsRepository';
 
 interface IRequest {
   name: string;
@@ -15,8 +16,23 @@ interface IRequest {
 class CreateProductService {
   constructor(private productsRepository: IProductsRepository) {}
 
+  @inject('ProductsRepository')
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
     // TODO
+    const productExists = await this.productsRepository.findByName(name);
+
+    if(productExists){
+      throw new AppError("There is already a product with this name.");
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
+
   }
 }
 
